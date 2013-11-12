f933a5cf7650702402f1201905007c01ece33204
Tony Pelletier <tony.pelletier@gmail.com>
2013-11-12 15:39:49 -0500

    changes

--- setup.py
+++ setup.py
@@ -1,13 +1,75 @@
 #!/usr/bin/env python
+# coding=utf-8
 
-from distutils.core import setup
+import os
+import sys
 
-setup(name='oemclient',
-	version='0.1',
-	description='Oemclient',
-	author='Tony Pelletier',
-	author_email='tony.pelletier@gmail.com',
-	packages = ['oemclient'],
-	package_data = {'package' : 'files'},
-	py_modules = ['oemclient'],
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    import multiprocessing
+except:
+    pass
+
+APP_NAME = 'oemclient'
+VERSION = '0.1.0'
+
+# Grab requirments.
+with open('requirements.txt') as f:
+    required = f.readlines()
+
+settings = dict()
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+# Build Helper.
+if sys.argv[-1] == 'build':
+    try:
+        import py2exe
+    except ImportError:
+        print('py2exe is required to continue.')
+        sys.exit(1)
+
+    sys.argv.append('py2exe')
+
+    settings.update(
+        zipfile = None,
+        options = {
+            'py2exe': {
+                'compressed': 1,
+                'optimize': 0,
+                'bundle_files': 1}})
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
+    author='Tony Pelletier',
+    author_email='tony.pelletier@gmail.com',
+    scripts=['oemclient.py'],
+    url='https://github.com/tpe11etier/oemclient',
+    license='MIT',
+    description='oemclient is a client for the Varolii xPress API',
+    long_description=open('README').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+    # install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'oemclient = oemclient:main',
+        ],
+    }
 )
+
+setup(**settings)
